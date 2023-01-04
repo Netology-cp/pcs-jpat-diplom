3c5ac825da4d1736c7ac2fe16b6041ddf14fe811
Nikolay Karmanov <karmnic@gmail.com>
2023-01-04 11:38:14 +0300

    added main part of code

--- src/main/java/ru/netology/javacore/Todos.java
+++ src/main/java/ru/netology/javacore/Todos.java
@@ -4,18 +4,41 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Todos {
-    //...
+    private Deque<List<String>> stack = new LinkedList<>();
+    private List<String> actual = new LinkedList<>();
 
     public void addTask(String task) {
-        //...
+        if (actual.size() < 8) {
+            // Перед выполнением действия сохраняем текущее состояние в стек
+            stack.add(actual.stream().collect(Collectors.toList()));
+            actual.add(task);
+        }
+        getAllTasks();
     }
 
     public void removeTask(String task) {
-        //...
+        if (actual.contains(task)) {
+            // Перед выполнением действия сохраняем текущее состояние в стек
+            stack.add(actual.stream().collect(Collectors.toList()));
+            actual.remove(task);
+        }
+        getAllTasks();
     }
 
     public String getAllTasks() {
-        //...
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String item : actual) {
+            if ((stringBuilder.toString().equals(""))) {
+                stringBuilder.append(item);
+            } else {
+                stringBuilder.append(" ").append(item);
+            }
+        }
+        return stringBuilder.toString();
     }
 
+    public void restoreTask() {
+        actual = stack.pollLast();
+        getAllTasks();
+    }
 }
